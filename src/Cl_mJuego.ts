bfe959620d55153665251e744f03f4ed2995cc95
Arráiz Eliézer <1001.30698890.ucla@gmail.com>
2025-11-12 11:28:36 -0400

    first commit

---
+++ src/Cl_mJuego.ts
@@ -0,0 +1,49 @@
+import Cl_mEquipo, { iEquipo } from "./Cl_mEquipo.js";
+export default class Cl_mJuego {
+  private equipos: Cl_mEquipo[] = [];
+  AgregarEquipo({
+    equipo,
+    callback,
+  }: {
+    equipo: Cl_mEquipo;
+    callback: (error: string | false) => void;
+  }): void {
+    const nombreRepetido = this.equipos.find(
+      (e) => e.nombreEquipo === equipo.nombreEquipo
+    );
+    if (nombreRepetido) {
+      callback("El nombre del equipo ya existe");
+      return;
+    }
+
+    const cedulaRepetida = equipo.error();
+
+    if (cedulaRepetida) {
+      callback("La cédula del jugador ya existe");
+      return;
+    }
+    // Validar cedulas contra todos los grupos existentes
+    for (const e of this.equipos) {
+      if (
+        e.existeCedula(equipo.cedula1) ||
+        e.existeCedula(equipo.cedula2) ||
+        e.existeCedula(equipo.cedula3) ||
+        e.existeCedula(equipo.cedula4)
+      ) {
+        callback(
+          `El equipo ${equipo.nombreEquipo} tiene cédula repetida con el equipo ${e.nombreEquipo}.`
+        );
+        return;
+      }
+    }
+
+    this.equipos.push(equipo);
+    localStorage.setItem("equipos", JSON.stringify(this.equipos));
+    callback(false);
+  }
+  listar(): iEquipo[] {
+    let equipos: iEquipo[] = [];
+    this.equipos.forEach((e) => equipos.push(e.toJSON()));
+    return equipos;
+  }
+}
